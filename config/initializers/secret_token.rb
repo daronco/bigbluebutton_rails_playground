2ef397e84b616965438084212514ba12d3597f10
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2011-03-16 16:55:03 -0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BigbluebuttonRailsPlayground::Application.config.secret_token = '1326240e06e62a21caf41042da5614fde76ae84fbdd39c3577d22e0c998bb0d776a79f60824ba4d8c02dbe575e9eecbeb19ba582411e2679869ac79a28d84f9d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BigbluebuttonRailsPlayground::Application.config.session_store :cookie_store, :key => '_bigbluebutton-rails-playground_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BigbluebuttonRailsPlayground::Application.config.session_store :active_record_store
