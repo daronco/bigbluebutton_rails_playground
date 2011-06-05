962bb97ce46755e6d9e55b491e0520cd6646f3ef
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2011-06-05 00:32:43 -0300

    New route to create rooms belonging to users and some fancy stuff to make everything prettier.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ BigbluebuttonRailsPlayground::Application.routes.draw do
   bigbluebutton_routes :default, :controllers => { :servers => 'custom_servers', :rooms => 'custom_rooms' }
 
   resources :users do
+    get :new_room, :to => 'custom_rooms#user_new', :as => 'new_room'
+
     bigbluebutton_routes :room_matchers
     resources :spaces do
       bigbluebutton_routes :room_matchers
