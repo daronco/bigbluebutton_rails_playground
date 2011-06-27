4dc7e28a0084ea20c2d2c5bb0545eee2b3dcf1f7
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2011-06-26 22:25:11 -0300

    Removing useless Space model (and everything related).

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,7 @@ BigbluebuttonRailsPlayground::Application.routes.draw do
 
   resources :users do
     get :new_room, :to => 'custom_rooms#user_new', :as => 'new_room'
-
     bigbluebutton_routes :room_matchers
-    resources :spaces do
-      bigbluebutton_routes :room_matchers
-    end
   end
 
   root :to => 'home#index'
