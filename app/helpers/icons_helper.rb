94df86bb44c0b3691ef073ac4d1b1e2cf6d3bf7b
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2012-11-25 02:32:43 -0200

    Adding basic css, for navbar and footer mainly

--- app/helpers/icons_helper.rb
+++ app/helpers/icons_helper.rb
@@ -2,7 +2,7 @@ module IconsHelper
 
   # Default icon that shows a tooltip with help about something
   def icon_help(title, options={})
-    cls = "icon-help tooltipped upwards "
+    cls = "icon icon-help tooltipped upwards "
     options[:class] = options.has_key?(:class) ? cls + options[:class] : cls
     options.merge!(:title => title)
     content_tag :div, nil, options
@@ -10,7 +10,7 @@ module IconsHelper
 
   # Default icon that shows a tooltip with information about something
   def icon_info(title, options={})
-    cls = "icon-info tooltipped upwards "
+    cls = "icon icon-info tooltipped upwards "
     options[:class] = options.has_key?(:class) ? cls + options[:class] : cls
     options.merge!(:title => title)
     content_tag :div, nil, options
